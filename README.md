c1b3e6b296d1c507baecac7bc6aa1de7b035e50b
weerapat-suriwong <122963159+weerapat-suriwong@users.noreply.github.com>
2023-09-05 09:13:52 +0000

    lab3_6531503123

--- README.md
+++ README.md
@@ -31,5 +31,5 @@ What benefit do we have from having integration tests in our project?
 ```
 Your though here
 ```
-
+Gain knowledge about coding bmi and creating a calculation system
 
--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -9,20 +9,35 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
+@WebServlet("/calbmi")
 public class BMICalculatorServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO: get parameter from request: "weight" and "height"
-        
+         double weight = Double.parseDouble(request.getParameter("weight"));
+         double height = Double.parseDouble(request.getParameter("height"));
         //TODO: calculate bmi
-
+         double bmi = weight / ((height ) * (height ));
+         int roundedBMI=(int)Math.round(bmi);
         //TODO: determine the built from BMI
-      
+      String builtType ="normal";
+      if(bmi<18.5){
+        builtType= "underweight";
+       } else if (bmi<25){
+       builtType= "normal";
+       }else if (bmi<30){
+        builtType =" overweight ";
+       }else if (bmi <35){
+        builtType = "obese";
+       }else{
+        builtType = "extremaly obese";
+       }
         //TODO: add bmi and built to the request's attribute
-
+request.setAttribute("bmi",roundedBMI);
+request.setAttribute("builtType",builtType);
         //TODO: forward to jsp
-           
+          request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);  
     }
     
 }
